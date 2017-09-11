7df93ad43eeb2ddc0ed802651880541f1ef7e932
Jim McCann (retchow) <ix@tchow.com>
2017-09-11 19:00:47 -0400

    starting on base2 with a scene description

---
+++ Scene.cpp
@@ -0,0 +1,68 @@
+#include "Scene.hpp"
+
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+glm::mat4 Scene::Transform::make_local_to_world() const {
+	return glm::mat4( //translate
+		glm::vec4(1.0f, 0.0f, 0.0f, 0.0f),
+		glm::vec4(0.0f, 1.0f, 0.0f, 0.0f),
+		glm::vec4(0.0f, 0.0f, 1.0f, 0.0f),
+		glm::vec4(position, 1.0f)
+	)
+	* glm::mat4_cast(rotation) //rotate
+	* glm::mat4( //scale
+		glm::vec4(scale.x, 0.0f, 0.0f, 0.0f),
+		glm::vec4(0.0f, scale.y, 0.0f, 0.0f),
+		glm::vec4(0.0f, 0.0f, scale.z, 0.0f),
+		glm::vec4(0.0f, 0.0f, 0.0f, 1.0f)
+	);
+}
+
+glm::mat4 Scene::Transform::make_world_to_local() const {
+	glm::vec3 inv_scale;
+	inv_scale.x = (scale.x == 0.0f ? 0.0f : 1.0f / scale.x);
+	inv_scale.y = (scale.y == 0.0f ? 0.0f : 1.0f / scale.y);
+	inv_scale.z = (scale.z == 0.0f ? 0.0f : 1.0f / scale.z);
+	return glm::mat4( //un-scale
+		glm::vec4(inv_scale.x, 0.0f, 0.0f, 0.0f),
+		glm::vec4(0.0f, inv_scale.y, 0.0f, 0.0f),
+		glm::vec4(0.0f, 0.0f, inv_scale.z, 0.0f),
+		glm::vec4(0.0f, 0.0f, 0.0f, 1.0f)
+	)
+	* glm::mat4_cast(glm::inverse(rotation)) //un-rotate
+	* glm::mat4( //un-translate
+		glm::vec4(1.0f, 0.0f, 0.0f, 0.0f),
+		glm::vec4(0.0f, 1.0f, 0.0f, 0.0f),
+		glm::vec4(0.0f, 0.0f, 1.0f, 0.0f),
+		glm::vec4(-position, 1.0f)
+	);
+}
+
+//---------------------------
+
+glm::mat4 Scene::Camera::make_projection() const {
+	return glm::infinitePerspective( fovy, aspect, near );
+}
+
+//---------------------------
+
+void Scene::render() {
+	for (auto const &object : objects) {
+		//compute modelview+projection matrix for this object:
+		glm::mat4 mvp =
+			  camera.make_projection()
+			* camera.transform.make_world_to_local()
+			* object.transform.make_local_to_world();
+
+		//set up program uniforms:
+		glUseProgram(object.program);
+		if (object.program_mvp != -1U) {
+			glUniformMatrix4fv(object.program_mvp, 1, GL_FALSE, glm::value_ptr(mvp));
+		}
+		glBindVertexArray(object.vao);
+
+		//draw the object:
+		glDrawArrays(GL_TRIANGLE_STRIP, object.start, object.count);
+	}
+}
---
+++ Scene.hpp
@@ -0,0 +1,50 @@
+#pragma once
+
+#include "GL.hpp"
+#include <glm/glm.hpp>
+#include <glm/gtc/quaternion.hpp>
+#include <vector>
+
+//Describes a 3D scene for rendering:
+struct Scene {
+	struct Transform {
+		glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f);
+		glm::quat rotation = glm::quat(0.0f, 0.0f, 0.0f, 1.0f);
+		glm::vec3 scale = glm::vec3(1.0f, 1.0f, 1.0f);
+
+		//computed from the above:
+		glm::mat4 make_local_to_world() const;
+		glm::mat4 make_world_to_local() const;
+	};
+	struct Camera {
+		Transform transform;
+		//camera parameters (perspective):
+		float fovy = 60.0f; //vertical fov (in radians)
+		float aspect = 1.0f; //x / y
+		float near = 0.01f; //near plane
+		//computed from the above:
+		glm::mat4 make_projection() const;
+	};
+	struct Object {
+		Transform transform;
+		//geometric info:
+		GLuint vao = 0;
+		GLuint start = 0;
+		GLuint count = 0;
+		//program info:
+		GLuint program = 0;
+		GLuint program_mvp = -1U; //uniform index for MVP matrix
+	};
+	struct Light {
+		Transform transform;
+		//light parameters (directional):
+		glm::vec3 intensity = glm::vec3(1.0f, 1.0f, 1.0f); //effectively, color
+		glm::vec3 direction = glm::vec3(0.0f,-1.0f, 0.0f);
+	};
+
+	Camera camera;
+	std::vector< Object > objects;
+	std::vector< Light > lights;
+
+	void render();
+};
